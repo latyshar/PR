30e3c883519bdb66bfdcbfa3daaa703fe8c0afed
latyshar <latysheva.yy@yandex.ru>
2023-08-28 20:09:55 +0300

    f

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -6,16 +6,17 @@ import {Link} from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="mainNav">
+    <div className="mainNav">
     <div className=" mainNav__logo ">
     <div className='up'>
     <div className="milky">1-ТИВ-3</div>
+    <div className='margin'>
     <Link className='text m' to="/raspisanie">Все дни</Link> 
-    <Link className='text m' to="/">Сегодня</Link>
+    <Link className='text m' to="/">Сегодня</Link></div>
     </div>
     </div>
 
- </nav>
+ </div>
   )
 }
 
